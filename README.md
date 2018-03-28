f26c4b9b8e904eda5b7b66ca2796a6f4f10a8290
Puneet Gupta <puneetgupta1987@gmail.com>
2018-03-28 19:13:49 +0100

    adding controller to return Hello World

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # basic-boot-web
+Register on Docker website
+Install Docker - https://docs.docker.com/docker-for-windows/install/#what-to-know-before-you-install
--- src/main/java/com/example/basicbootweb/BasicBootWebApplication.java
+++ src/main/java/com/example/basicbootweb/BasicBootWebApplication.java
@@ -2,11 +2,20 @@ package com.example.basicbootweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BasicBootWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicBootWebApplication.class, args);
+	}
+}
+@RestController
+class HelloWorldController{
+	@RequestMapping("/")
+	String helloWorld(){
+		return "HelloWorld";
 	}
 }
