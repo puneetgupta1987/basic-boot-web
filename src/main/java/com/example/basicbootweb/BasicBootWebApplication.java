eca1eb2a50adfd41afb97638d433204f2885c0f4
Puneet Gupta <puneetgupta1987@gmail.com>
2018-03-28 18:14:29 +0100

    First commit

---
+++ src/main/java/com/example/basicbootweb/BasicBootWebApplication.java
@@ -0,0 +1,12 @@
+package com.example.basicbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicBootWebApplication.class, args);
+	}
+}
